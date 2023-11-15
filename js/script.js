9505188a579cbf109d2912887412c28c0e131be2
akgeninov <akgeninov@gmail.com>
2023-11-15 18:22:49 +0700

    feat: up file

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const logRegBox = document.querySelector('.logreg-box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const registerWithEmail = document.querySelector('.btn-email');
+const backToRegist1 = document.querySelector('.arrow-back');
+
+registerLink.addEventListener('click', () => {
+    logRegBox.classList.add('active');
+});
+loginLink.addEventListener('click', () => {
+    logRegBox.classList.remove('active');
+});
+registerWithEmail.addEventListener('click', () => {
+    logRegBox.classList.add('active2');
+    logRegBox.classList.remove('active');
+});
+backToRegist1.addEventListener('click', () => {
+    logRegBox.classList.add('active');
+    logRegBox.classList.remove('active2');
+});

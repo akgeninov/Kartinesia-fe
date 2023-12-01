ff6e5ce0f88131f0daa66f7f25143f830f2719a9
akgeninov <akgeninov@gmail.com>
2023-12-01 07:07:12 +0700

    fix: nav responsive

--- js/home.js
+++ js/home.js
@@ -24,3 +24,10 @@ function toggleDropdown() {
       }
     }
   }
+
+const menuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+  nav.classList.toggle('menu');
+});

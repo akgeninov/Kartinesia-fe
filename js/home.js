127e13cef9058cbda8c9bb316a5b8949c0bab3e9
Rida Zaimus <ridazaimus15@gmail.com>
2023-12-07 22:33:23 +0800

    feat: fetch

--- js/home.js
+++ js/home.js
@@ -31,3 +31,36 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function(){
   nav.classList.toggle('menu');
 });
+
+fetch("http://localhost:3600/articles?category=fesyen")
+.then(res => res.json())
+.then(data => renderDataToContent(data.data));
+
+function renderDataToContent(articles){
+    // get element content
+    // let content = document.getElementById("content");
+    // let id = 1;
+    // for (article of articles){
+    //     console.log(article);
+    //     content.innerHTML+=`<div class="card" id=${id}>
+    // <div class="thumb">
+    //     <img src=${article.urlImage} alt="">
+    // </div>
+    // <div class="title">
+    //     <a onclick="redirectToDetail(${id})">${article.title}</a>
+    // </div>
+    // </div>`
+    //     id++;
+    // }
+    console.log(articles)
+    // get parent element
+    let parent = document.getElementById("list-news-right")
+    for (article of articles){
+      parent.innerHTML+=
+      `<div class="image-container">
+                <img src=${article.url_image} alt="">
+                <div class="overlay-text">${article.title}</div>
+              </div>`
+    }
+    
+}

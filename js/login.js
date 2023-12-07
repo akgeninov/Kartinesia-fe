3c7b0ca60c0fa25b9d4f31091002349dfb50f0ca
akgeninov <akgeninov@gmail.com>
2023-12-07 18:07:04 +0700

    fix

--- js/login.js
+++ js/login.js
@@ -1,20 +1,20 @@
 const logRegBox = document.querySelector('.logreg-box');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const registerWithEmail = document.querySelector('.btn-email');
-const backToRegist1 = document.querySelector('.arrow-back');
+// const registerWithEmail = document.querySelector('.btn-email');
+// const backToRegist1 = document.querySelector('.arrow-back');
 
 registerLink.addEventListener('click', () => {
     logRegBox.classList.add('active');
 });
 loginLink.addEventListener('click', () => {
     logRegBox.classList.remove('active');
-});
-registerWithEmail.addEventListener('click', () => {
-    logRegBox.classList.add('active2');
-    logRegBox.classList.remove('active');
-});
-backToRegist1.addEventListener('click', () => {
-    logRegBox.classList.add('active');
-    logRegBox.classList.remove('active2');
-});
+}); 
+// registerWithEmail.addEventListener('click', () => {
+//     logRegBox.classList.add('active2');
+//     logRegBox.classList.remove('active');
+// });
+// backToRegist1.addEventListener('click', () => {
+//     logRegBox.classList.add('active');
+//     logRegBox.classList.remove('active2');
+// });

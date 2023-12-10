de5fab5f27d799de790e0a98a05fb2826a470757
Rida Zaimus <ridazaimus15@gmail.com>
2023-12-10 23:13:15 +0800

    detail berita

--- js/content.js
+++ js/content.js
@@ -45,3 +45,155 @@ function toggleLove(loveBtn) {
     loveBtn.classList.toggle('active');
     likeCountElement.textContent = loves;
 }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("DOM Content Loaded");
+    // Get the product ID from the URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const newsId = urlParams.get('article_id');
+    
+    console.log(newsId);
+    
+    // Get selected news
+    fetch(`https://sleepy-jay-bandanna.cyclic.app/news/${newsId}`)
+    .then(res => res.json())
+    .then(data => renderDataToDetail(data));
+
+    function renderDataToDetail(article){
+    console.log(article)
+    // get parent element
+    let parent = document.getElementById("detail-news")
+      parent.innerHTML+=
+      `<div class="header-news">
+                <h2>${article.title}</h2>
+                <img src=${article.url_image} alt="">
+                <div class="share">
+                    <a href=""><img src="/asset/icon _Instagram icon_.png" alt=""></a></a>
+                    <a href=""><img src="/asset/Facebook v1 icon_.png" alt=""></a>
+                    <a href=""><img src="/asset/icon _Whatsapp_.png" alt=""></a>
+                    <a href=""><img src="/asset/icon _Twitter_.png" alt=""></a>
+                    <a href=""><img src="/asset/icon_link.png" alt=""></a>
+                </div>
+            </div>
+            <div class="contain">
+                <p>Kartinesia. - ${article.description}</p>
+            </div>
+            <div class="baca-juga">
+                <span>Baca juga :</span>
+                <a href="">5 Cara Mengatasi Lonjakan Gula Darah Sebelum dan Sesudah Makan Siang</a>
+            </div>
+            <div class="artikel-terkait">
+                <span class="judul">artikel terkait</span>
+                <div class="list-artikel-terkait">
+                    <div class="artikel">
+                        <img src="asset/artikel.png" alt="">
+                        <div class="text">
+                            <span>Bahaya Gula Pasir Berlebihan Menurut dr Tirta</span>
+                        </div>
+                    </div>
+                    <div class="artikel">
+                        <img src="asset/artikel.png" alt="">
+                        <div class="text">
+                            <span>Bahaya Gula Pasir Berlebihan Menurut dr Tirta</span>
+                        </div>
+                    </div>
+                    <div class="artikel">
+                        <img src="asset/artikel.png" alt="">
+                        <div class="text">
+                            <span>Bahaya Gula Pasir Berlebihan Menurut dr Tirta</span>
+                        </div>
+                    </div>
+                    <div class="artikel">
+                        <img src="asset/artikel.png" alt="">
+                        <div class="text">
+                            <span>Bahaya Gula Pasir Berlebihan Menurut dr Tirta</span>
+                        </div>
+                    </div>
+                    <div class="artikel">
+                        <img src="asset/artikel.png" alt="">
+                        <div class="text">
+                            <span>Bahaya Gula Pasir Berlebihan Menurut dr Tirta</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="comment">
+                <span>Komentar</span>
+                <div class="contain-comment">
+                    <div class="comment-bar">
+                        <form action="" class="box-comment">
+                            <input type="text" placeholder="Tuliskan komentar Anda" name="search-bar">
+                        </form>
+                        <button>Kirim</button>
+                        <span>*Isi komentar sepenuhnya adalah tanggung jawab pengguna</span>
+                    </div>
+                    <div class="info">
+                        <span>3 komentar</span>
+                        <div class="filter" id="filter">
+                            <button class="filter-btn selected" onclick="applyFilter('terbaru')">terbaru</button>
+                            <button class="filter-btn" onclick="applyFilter('terlama')">terlama</button>
+                            <button class="filter-btn" onclick="applyFilter('terpopuler')">terpopuler</button>
+                        </div>
+                    </div>
+                    <div id="comments">
+                        <div class="komentar" data-date="2022-08-24" data-likes="0">
+                            <div class="img">
+                                <img src="asset/profile.png" alt="">
+                            </div>
+                            <div class="sender-date-contain-like">
+                                <div class="name-date">
+                                    <span>Anonim</span>
+                                    <span class="comment-date">24 Agustus 2022</span>
+                                </div>
+                                <div class="bunyi-komentar">
+                                    <span>Sangat membantu</span>
+                                </div>
+                                <div class="like">
+                                    <span class="love-btn" onclick="toggleLove(this)"><i class="far fa-heart"></i></span>
+                                    <span class="like-count">0</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="komentar" data-date="2023-03-01" data-likes="0">
+                            <div class="img">
+                                <img src="asset/profile.png" alt="">
+                            </div>
+                            <div class="sender-date-contain-like">
+                                <div class="name-date">
+                                    <span>Anonim</span>
+                                    <span class="comment-date">1 Maret 2023</span>
+                                </div>
+                                <div class="bunyi-komentar">
+                                    <span>Berita yang menarik</span>
+                                </div>
+                                <div class="like">
+                                    <span class="love-btn" onclick="toggleLove(this)"><i class="far fa-heart"></i></span>
+                                    <span class="like-count">0</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="komentar" data-date="2023-11-01" data-likes="0">
+                            <div class="img">
+                                <img src="asset/profile.png" alt="">
+                            </div>
+                            <div class="sender-date-contain-like">
+                                <div class="name-date">
+                                    <span>Anonim</span>
+                                    <span class="comment-date">1 November 2023</span>
+                                </div>
+                                <div class="bunyi-komentar">
+                                    <span>Relate banget untuk jaman sekarang</span>
+                                </div>
+                                <div class="like">
+                                    <span class="love-btn" onclick="toggleLove(this)"><i class="far fa-heart"></i></span>
+                                    <span class="like-count">0</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`        
+        }
+    }
+);
--- js/home.js
+++ js/home.js
@@ -45,9 +45,10 @@ function renderDataToContent(articles){
     for (article of limitedArticles){
       parent.innerHTML+=
       `<div class="image-container">
-                <a href="content.html"><img src=${article.url_image} alt=""></a>
-                <a href="content.html">
-                <div class="overlay-text">${article.title}</div></a>
+                <a onclick="redirectToDetail(${article.article_id})" style="cursor: pointer;"><img src=${article.url_image} alt=""></a>
+                <div class="overlay-text">
+                  <a onclick="redirectToDetail(${article.article_id})" style="cursor: pointer;">${article.title}</a>
+                </div>
               </div>`
     }
 }
@@ -67,16 +68,24 @@ function renderDataToContentKesehatan(articles){
       const truncatedDescription = article.description.slice(0, maxDescriptionLength);
       parent.innerHTML+=
       `<div class="picture pct1">
-      <a href="content.html"><img src=${article.url_image} alt=""></a>    
-      <a href="content.html">
+        <a onclick="redirectToDetail(${article.article_id})" style="cursor: pointer;"><img src=${article.url_image} alt=""></a>    
         <div class="text">
-          <span>${article.title}</span>
+          <span>
+            <a onclick="redirectToDetail(${article.article_id})" style="cursor: pointer;">${article.title}</a>
+          </span>
           <span>${truncatedDescription}...</span> <!-- Menampilkan description yang telah dipotong -->
         </div>
-      </a>
   </div>`
     }
-}
+  }
+  
+  // function untuk direct ke content.html
+  function redirectToDetail(newsId) {
+    console.log(newsId)
+    console.log('redirect to content');
+    // You can use window.location.href to redirect to the detail page
+    window.location.href = 'content.html?article_id=' + newsId;
+  }
 
 // fetch("https://sleepy-jay-bandanna.cyclic.app/articles?category=fesyen")
 // .then(res => res.json())
@@ -112,55 +121,49 @@ function renderDataToContentKesehatan(articles){
 //     }
 // }
 
-// function untuk direct ke content.html
-function redirectToDetail(article_id) {
-  console.log('redirect to content');
-  // You can use window.location.href to redirect to the detail page
-  window.location.href = 'content.html?id=' + article_id;
-}
 
 // Misalkan terdapat sebuah form pencarian pada halaman HTML dengan id "searchForm" dan input dengan id "searchInput"
-const searchForm = document.getElementById('searchForm');
-const searchInput = document.getElementById('searchInput');
+// const searchForm = document.getElementById('searchForm');
+// const searchInput = document.getElementById('searchInput');
 
-searchForm.addEventListener('submit', function (event) {
-  event.preventDefault(); // Mencegah form untuk melakukan submit default
+// searchForm.addEventListener('submit', function (event) {
+//   event.preventDefault(); // Mencegah form untuk melakukan submit default
 
-  const searchTerm = searchInput.value;
+//   const searchTerm = searchInput.value;
 
-  fetch(`https://sleepy-jay-bandanna.cyclic.app/articles/search?q=${searchTerm}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      // Data yang diterima dari server
-      console.log('Search Results:', data.data); // Menampilkan data pencarian di konsol
+//   fetch(`https://sleepy-jay-bandanna.cyclic.app/articles/search?q=${searchTerm}`)
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+//       return response.json();
+//     })
+//     .then(data => {
+//       // Data yang diterima dari server
+//       console.log('Search Results:', data.data); // Menampilkan data pencarian di konsol
 
-      // Lakukan sesuatu dengan data pencarian di sini
-      // Misalnya, tampilkan hasil pencarian ke dalam sebuah div di halaman web
-      displaySearchResults(data.data);
-    })
-    .catch(error => {
-      // Menampilkan pesan kesalahan jika terjadi error
-      console.error('There was a problem with the fetch operation:', error);
-      // Lakukan penanganan error sesuai kebutuhan
-    });
-});
+//       // Lakukan sesuatu dengan data pencarian di sini
+//       // Misalnya, tampilkan hasil pencarian ke dalam sebuah div di halaman web
+//       displaySearchResults(data.data);
+//     })
+//     .catch(error => {
+//       // Menampilkan pesan kesalahan jika terjadi error
+//       console.error('There was a problem with the fetch operation:', error);
+//       // Lakukan penanganan error sesuai kebutuhan
+//     });
+// });
 
-function displaySearchResults(results) {
-  // Lakukan sesuatu dengan hasil pencarian yang diterima dari server
-  // Misalnya, menambahkan hasil pencarian ke dalam sebuah div di halaman web
-  const resultsDiv = document.getElementById('searchResults');
+// function displaySearchResults(results) {
+//   // Lakukan sesuatu dengan hasil pencarian yang diterima dari server
+//   // Misalnya, menambahkan hasil pencarian ke dalam sebuah div di halaman web
+//   const resultsDiv = document.getElementById('searchResults');
 
-  resultsDiv.innerHTML = ''; // Kosongkan isi div sebelum menambahkan hasil pencarian baru
+//   resultsDiv.innerHTML = ''; // Kosongkan isi div sebelum menambahkan hasil pencarian baru
 
-  results.forEach(result => {
-    const resultElement = document.createElement('div');
-    resultElement.textContent = result.title; // Asumsi ada properti 'title' di setiap artikel
+//   results.forEach(result => {
+//     const resultElement = document.createElement('div');
+//     resultElement.textContent = result.title; // Asumsi ada properti 'title' di setiap artikel
 
-    resultsDiv.appendChild(resultElement);
-  });
-}
+//     resultsDiv.appendChild(resultElement);
+//   });
+// }

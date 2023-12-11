57386aca1e36f247eef90e3efac2b933ba065404
Rida Zaimus <ridazaimus15@gmail.com>
2023-12-11 17:22:12 +0800

    maaf telat kak, kendala mati lampu

--- js/profile.js
+++ js/profile.js
@@ -1,55 +1,46 @@
-document.addEventListener('DOMContentLoaded', function () {
-  console.log("DOM Content Loaded");
-  // Get the product ID from the URL
-  // const urlParams = new URLSearchParams(window.location.search);
-  // const newsId = urlParams.get('article_id');
+// document.addEventListener('DOMContentLoaded', function () {
+//   console.log("DOM Content Loaded");
+//   // Get the product ID from the URL
+//   // const urlParams = new URLSearchParams(window.location.search);
+//   // const newsId = urlParams.get('article_id');
   
-  // console.log(newsId);
+//   // console.log(newsId);
   
-  // Get selected news
-  fetch(`https://sleepy-jay-bandanna.cyclic.app/users`)
-  .then(res => res.json())
-  .then(data => renderDataToDetail(data));
+//   // Get selected news
+//   fetch(`https://sleepy-jay-bandanna.cyclic.app/users`)
+//   .then(res => res.json())
+//   .then(data => renderDataToDetail(data));
 
-  function renderDataToDetail(user){
-  console.log(user)
-  // const randomId = Math.floor(Math.random() * 20) + 1;
-  console.log(user.email)
-  console.log(user.username)
-  // get parent element
-  let parent = document.getElementById("data-user")
-    parent.innerHTML+=
-    `<table>
-    <tr>
-      <td>Email</td>
-      <td> : </td>
-      <td>${user.email}</td>
-    </tr>
-    <tr>
-      <td>Nama pengguna</td>
-      <td> : </td>
-      <td>${user.username}</td>
-    </tr>
-  </table>`      
-      }
-  }
-);
+//   function renderDataToDetail(user){
+//   console.log(user)
+//   // const randomId = Math.floor(Math.random() * 20) + 1;
+//   console.log(user.email)
+//   console.log(user.username)
+//   // get parent element
+//   let parent = document.getElementById("data-user")
+//     parent.innerHTML+=
+//     `<table>
+//     <tr>
+//       <td>Email</td>
+//       <td> : </td>
+//       <td>${user.email}</td>
+//     </tr>
+//     <tr>
+//       <td>Nama pengguna</td>
+//       <td> : </td>
+//       <td>${user.username}</td>
+//     </tr>
+//   </table>`      
+//       }
+//   }
+// );
 
 document.addEventListener('DOMContentLoaded', function() {
-  const logoutButton = document.getElementById('logout-btn');
-  // Cek apakah ada token yang tersimpan di local storage
-  const token = localStorage.getItem('token');
-  logoutButton.addEventListener('click', function() {
-  });
-  if (token && token !== 'undefined') {
-    console.log(token);
-    // Jika ada token, ubah teks tombol dan href-nya
-    // loginButton.textContent = 'PROFILE';
-    loginButton.addEventListener('click', function() {
+  const logoutButton = document.getElementById('logoutbtn');
+    logoutButton.addEventListener('click', function() {
       window.location.href = 'index.html';
       localStorage.removeItem('token');
     });
-    };
   });
   // if(token !== 'undefined'){
   //   // alert('Pengguna tidak ditemukan');
